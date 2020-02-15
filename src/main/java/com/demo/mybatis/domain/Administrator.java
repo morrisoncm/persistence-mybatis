9f16855734da2b7997ae2a4cd099033b7955a963
CLinton Morrison <morrison.clinton@gmail.com>
2020-02-15 13:39:35 -0500

    GIT ignore files

--- src/main/java/com/demo/mybatis/domain/Administrator.java
+++ src/main/java/com/demo/mybatis/domain/Administrator.java
@@ -2,19 +2,20 @@ package com.demo.mybatis.domain;
 
 public class Administrator {
 
-	private String id;
+	private String administratorId;
 	private String username;
 	private String password;
 	private String firstname;
 	private String lastname;
 	private String email;
+	private Role role;
 
-	public String getId() {
-		return id;
+	public String getAdministratorId() {
+		return administratorId;
 	}
 
-	public void setId(String id) {
-		this.id = id;
+	public void setAdministratorId(String administratorId) {
+		this.administratorId = administratorId;
 	}
 
 	public String getUsername() {
@@ -57,14 +58,19 @@ public class Administrator {
 		this.email = email;
 	}
 
+	public Role getRole() {
+		return role;
+	}
+
+	public void setRole(Role role) {
+		this.role = role;
+	}
+
 	@Override
 	public int hashCode() {
 		final int prime = 31;
 		int result = 1;
-		result = prime * result + ((email == null) ? 0 : email.hashCode());
-		result = prime * result + ((firstname == null) ? 0 : firstname.hashCode());
-		result = prime * result + ((lastname == null) ? 0 : lastname.hashCode());
-		result = prime * result + ((password == null) ? 0 : password.hashCode());
+		result = prime * result + ((username == null) ? 0 : username.hashCode());
 		return result;
 	}
 
@@ -77,30 +83,12 @@ public class Administrator {
 		if (getClass() != obj.getClass())
 			return false;
 		Administrator other = (Administrator) obj;
-		if (email == null) {
-			if (other.email != null)
+		if (username == null) {
+			if (other.username != null)
 				return false;
-		} else if (!email.equals(other.email)) {
+		} else if (!username.equals(other.username))
 			return false;
-		}
-		if (firstname == null) {
-			if (other.firstname != null)
-				return false;
-		} else if (!firstname.equals(other.firstname)) {
-			return false;
-		}
-		if (lastname == null) {
-			if (other.lastname != null)
-				return false;
-		} else if (!lastname.equals(other.lastname)) {
-			return false;
-		}
-		if (password == null) {
-			if (other.password != null)
-				return false;
-		} else if (!password.equals(other.password)) {
-			return false;
-		}
 		return true;
 	}
+
 }
--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -8,3 +8,27 @@ CREATE TABLE administrator (
   lastname VARCHAR(50) NOT NULL,
   email VARCHAR(75) NOT NULL
 );
+
+CREATE TABLE role (
+  id INT AUTO_INCREMENT  PRIMARY KEY,
+  role_name VARCHAR(50) NOT NULL,
+ role_description VARCHAR(100) NOT NULL
+);
+
+CREATE TABLE administrator_role (
+  id INT AUTO_INCREMENT  PRIMARY KEY,
+  administratorId INT, FOREIGN KEY (administratorId) REFERENCES administrator(id),
+  roleId INT, FOREIGN KEY (roleId) REFERENCES role(id)
+);
+
+CREATE TABLE privilege (
+  id INT AUTO_INCREMENT  PRIMARY KEY,
+  privilege_name VARCHAR(50) NOT NULL,
+  privilege_description VARCHAR(100) NOT NULL
+);
+
+CREATE TABLE role_ privilege (
+  id INT AUTO_INCREMENT  PRIMARY KEY,
+  roleId INT, FOREIGN KEY (roleId) REFERENCES role(id),
+  privilegeId INT, FOREIGN KEY (privilegeId) REFERENCES privilege(id)
+);
